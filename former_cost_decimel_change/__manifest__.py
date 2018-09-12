82d10cf8cf2eba86fd2c615cbfe71292123e3def
Amr <amr@192.168.1.52>
2018-09-12 14:11:05 +0200

    Former_cost_test2

--- former_cost_decimel_change/__manifest__.py
+++ former_cost_decimel_change/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['stock.valuation.adjustment.lines'],
+    'depends': ['stock.move'],
 
     # always loaded
     'data': [
--- former_cost_decimel_change/models/stock.valuation.adjustment.lines_inherit.py
+++ former_cost_decimel_change/models/stock.valuation.adjustment.lines_inherit.py
@@ -4,6 +4,8 @@ from odoo import models, fields
 
 class StockvaluationInherit(models.Model):
     _inherit = 'stock.valuation.adjustment.lines'
+    _inherit = 'stock.move'
+
 
 former_cost_per_unit = fields.Float('Former Cost(Per Unit)', compute='_compute_former_cost_per_unit',
     digits=dp.get_precision('Product Price'), store=True)
