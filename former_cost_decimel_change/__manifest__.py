78ad9ad16d45c2da4a4b3d0b9e97967292efc7f0
Amr <amr@192.168.1.52>
2018-09-12 13:54:38 +0200

    former_cost_modification

---
+++ former_cost_decimel_change/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock Valuation Former Cost Decimals Modification Module",
+
+    'summary': """
+        This module is a modification of Stock Valuation former cost per unit to product Price decimals on Odoo 11""",
+
+    'description': """
+        This module is a modification of Stock Valuation former cost per unit to product Price decimals on Odoo 11
+    """,
+
+    'author': "Israa Islam",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['stock.valuation.adjustment.lines'],
+
+    # always loaded
+    'data': [
+
+    ],
+}
